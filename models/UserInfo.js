75418e17432dae94d746cabeded68de199f9e3c9
megu-s <s2017045@gmail.com>
2025-03-10 15:51:13 +0800

    schema and hash password

--- models/UserInfo.js
+++ models/UserInfo.js
@@ -1,24 +1,25 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userInfoSchema = new Schema({
     username:{
         type : String,
-        required : true,
+        required : [true, 'Please enter an username'],
     },
     email:{
         type : String,
         required : true,
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email'],
     },
     password:{
         type : String,
-        required : true,
+        required: [true, 'Please enter a password'],
     },
     incomes:{
-        type : Array,
-        required : false,
-    },
-    income_values:{
         type : Array,
         required : false,
     },
@@ -26,11 +27,14 @@ const userInfoSchema = new Schema({
         type : Array,
         required : false,
     },
-    expense_values:{
-        type : Array,
-        required : false,
-    },
 }, {timestamps: false});
+
+userInfoSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
 
 const UserInfo = mongoose.model('user_infos', userInfoSchema);
 module.exports = UserInfo;
