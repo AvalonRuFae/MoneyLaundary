134f8315824ce2218924d4a74119b02a39c7f6a9
megu-s <s2017045@gmail.com>
2025-03-15 22:09:57 +0800

    debug

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -7,5 +7,6 @@ router.post('/main/addItem', Controller.addItem_post);
 
 router.delete('/main/deleteItem', Controller.deleteItem_delete);
 
-router.post('/main/addValue', Controller.addValue_post);
+router.post('/main/addAmount', Controller.addAmount_post);
 
+module.exports = router;
